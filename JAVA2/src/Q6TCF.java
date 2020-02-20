7f80c4678e60d63b981476501f17f911d95334cf
suraj <surajkd050599@gmail.com>
2020-02-20 09:07:41 +0530

    Q6

---
+++ JAVA2/src/Q6TCF.java
@@ -0,0 +1,24 @@
+public class Q6TCF {
+    public static void main (String args[])
+    {
+        try
+        {
+            throw new ClassNotFoundException("no class");
+        }
+        catch (ClassNotFoundException c)
+        {
+            System.out.println("Exception: "+c.getMessage());
+        }
+        try
+        {
+            throw new NoClassDefFoundError("no class Definition");
+        }
+        catch (NoClassDefFoundError n)
+        {
+            System.out.println("Exception: "+n.getMessage());
+        }
+        finally {
+            System.out.println("THE END OF PROGRAM");
+        }
+    }
+}
