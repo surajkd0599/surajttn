6971d1f833c89027984b215d3f8b6168dbe70cd6
suraj <surajkd050599@gmail.com>
2020-04-08 20:27:58 +0530

    Password controller for user updation added

--- ecommerce/src/main/java/com/example/project/ecommerce/controller/AppController.java
+++ ecommerce/src/main/java/com/example/project/ecommerce/controller/AppController.java
@@ -1,16 +1,20 @@
 package com.example.project.ecommerce.controller;
 
 import com.example.project.ecommerce.services.AppUserDetailsService;
+import com.example.project.ecommerce.services.PasswordService;
+import com.example.project.ecommerce.validator.EmailValidation;
+import com.example.project.ecommerce.validator.PasswordValidation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 
-@RequestMapping("/ecommerce")
 @RestController
+@RequestMapping("/ecommerce/app")
 public class AppController {
 
     @Autowired
@@ -18,6 +22,15 @@ public class AppController {
 
     @Autowired
     private TokenStore tokenStore;
+
+    @Autowired
+    private PasswordService passwordService;
+
+    @Autowired
+    private EmailValidation emailValidation;
+
+    @Autowired
+    private PasswordValidation passwordValidation;
 
     @GetMapping("/doLogout")
     public String logout(HttpServletRequest request){
@@ -28,6 +41,35 @@ public class AppController {
             tokenStore.removeAccessToken(accessToken);
         }
         return "Logged out successfully";
+    }
+
+    @PostMapping("/getToken/{email}")
+    public String getToken(@PathVariable("email") String email, HttpServletResponse response){
+        if(emailValidation.validateEmail(email)) {
+            String message = passwordService.sendToken(email);
+            if(!message.equals("Change your password")){
+                response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            }
+            return message;
+        }else {
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            return "Email is not valid";
+        }
+    }
+
+    @PatchMapping("/reset-password")
+    public String resetPassword(@RequestParam("token") String token, @RequestParam("email") String email,
+                                @RequestParam String oldPass, String newPass, String confirmPass, HttpServletResponse response){
+        if(passwordValidation.validatePassword(oldPass,newPass,confirmPass)){
+            String message = passwordService.resetPassword(email,token,oldPass,newPass,confirmPass);
+            if(!message.equals("Password successfully changed")){
+                response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            }
+            return message;
+        }else {
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            return "Password must be matched or password must be of minimum 8 characters and maximum 15 characters and must contain 1 uppercase letter,1 lowercase letter,1 digit and 1 special character";
+        }
     }
 
     @GetMapping("/")
